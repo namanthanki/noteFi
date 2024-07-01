50690aadc339e0bb22d81d449e4b8a9da0a8a44c
Crostons <shubh1608003@gmail.com>
2024-07-01 16:23:33 +0530

    Folder changes

--- contracts/src/secondary/OfferInterface.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface OptionContract {
-    function buyer() external view returns (address);
-
-    function transfer(address newBuyer) external;
-}
