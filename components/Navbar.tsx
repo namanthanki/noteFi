2eda9f4f45190772391644a877a9e614a899df81
Crostons <shubh1608003@gmail.com>
2024-07-15 16:48:41 +0530

    Navbar fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,6 +25,7 @@ function useHideOnScroll() {
 
 export default function Navbar() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const showNavbar = useHideOnScroll();
 
   const toggleDropdown = () => {
@@ -34,6 +35,7 @@ export default function Navbar() {
   const closeDropdown = () => {
     setDropdownOpen(false);
   };
+  
 
   return (
     <nav className={`fixed w-full z-20 top-0 bg-gray-900 border-b border-gray-600 transition-transform duration-300 ${showNavbar ? 'translate-y-0' : '-translate-y-full'}`}>
@@ -46,29 +48,24 @@ export default function Navbar() {
           <div className="px-4">
             <w3m-button label="Connect Wallet" /> {/* Assuming w3m-button is a component you have */}
           </div>
-          <button
-            data-collapse-toggle="navbar-hamburger"
-            type="button"
-            className="inline-flex items-center p-2 text-gray-400 rounded-lg lg:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600"
-            aria-controls="navbar-hamburger"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
+          <button className="inline-flex items-center p-2 ml-3 text-sm text-gray-400 rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600"
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
+              </svg>
+            </button>
         </div>
         <div className="hidden items-center justify-end w-full lg:flex lg:w-auto lg:order-1" id="navbar-sticky">
           <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
             <Link href="/">
-              <li className="block py-2 pr-4 pl-3 text-white hover:bg-gray-700 lg:hover:bg-transparent lg:p-0" onClick={closeDropdown}>
+              <li className="block py-2 pr-4 pl-3 text-white hover:bg-gray-700 lg:px-4 rounded-md hover:text-emerald-500" onClick={closeDropdown}>
                 Home
               </li>
             </Link>
             <li className="relative">
               <div
-                className={`flex items-center cursor-pointer py-2 pr-4 pl-3 text-white hover:bg-gray-700 lg:hover:bg-transparent lg:p-0 ${dropdownOpen ? 'text-white' : ''}`}
+                className={`flex items-center cursor-pointer py-2 pr-4 pl-3 text-white hover:bg-gray-700 lg:px-4 rounded-md hover:text-emerald-500 ${dropdownOpen ? 'text-white' : ''}`}
                 onClick={toggleDropdown}
               >
                 Options
@@ -85,12 +82,12 @@ export default function Navbar() {
                 <div className="mt-8 absolute z-10 bg-gray-800 rounded-lg shadow w-44">
                   <ul className="py-2 text-sm text-white">
                     <Link href="/write">
-                      <li className="block px-4 py-2 hover:bg-gray-700" onClick={closeDropdown}>
+                      <li className="block px-4 py-2 hover:bg-gray-700 hover:text-emerald-500" onClick={closeDropdown}>
                         Write Option
                       </li>
                     </Link>
                     <Link href="/buy">
-                      <li className="block px-4 py-2 hover:bg-gray-700" onClick={closeDropdown}>
+                      <li className="block px-4 py-2 hover:bg-gray-700 hover:text-emerald-500" onClick={closeDropdown}>
                         Buy Option
                       </li>
                     </Link>
@@ -99,13 +96,31 @@ export default function Navbar() {
               )}
             </li>
             <Link href="/positions">
-              <li className="block py-2 pr-4 pl-3 text-white hover:bg-gray-700 lg:hover:bg-transparent lg:p-0" onClick={closeDropdown}>
+              <li className="block py-2 pr-4 pl-3 text-white hover:bg-gray-700 lg:px-4 rounded-md hover:text-emerald-500" onClick={closeDropdown}>
                 Positions
               </li>
             </Link>
           </ul>
         </div>
       </div>
+      {isDropdownOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link href="/">
+              <div className="hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium text-emerald-500">Home</div>
+            </Link>
+            <Link href="/write">
+              <div className="hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium text-emerald-500">Write Option</div>
+            </Link>
+            <Link href="/buy">
+              <div className="hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium text-emerald-500">Buy an Option</div>
+            </Link>
+            <Link href="/positions">
+              <div className="hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium text-emerald-500">Positions</div>
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
