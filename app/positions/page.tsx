332cf73c892128b9e435e8e5052a4e76cf1f95d2
Shubh <110349596+Crosstons@users.noreply.github.com>
2024-07-18 21:20:49 +0530

    Positions Page Updated (#29)
    fixes #15

--- app/positions/page.tsx
+++ app/positions/page.tsx
@@ -66,26 +66,42 @@ const PositionsPage = () => {
 
   const renderPosition = (position: PositionData) => (
     <div className="my-8">
-    <div className="bg-gray-900 p-6 rounded-md text-white shadow-md">
+    <div className=" p-6 rounded-md text-white shadow-md bg-gray-800">
     <div className="text-xl font-semibold">
-      <span>{position.tokenName}</span> {' '}
-      <span>${position.strikePrice}</span> {' '}
+      <span className='text-2xl'>{position.tokenName}</span> {' '}
       {position.type === 'CALL' ? (
         <span className='ml-2 px-2 py-2 rounded-md bg-gray-800 border-[0.5px] text-green-400 border-green-200 text-sm'>{position.type}</span>
       ) : (
         <span className='ml-2 px-4 py-2 rounded-md bg-gray-800 border-[0.5px] text-red-400 text-sm border-red-200 '>{position.type}</span>
       )}
     </div>
-      <div className="flex justify-between items-center mt-2">
-        <div>
-          <div className='text-gray-300'>Quantity: {position.quantity}</div>
-          <div className='text-gray-300'>Expires on: {position.expiration}</div>
-          <div className='text-gray-300'>{position.positionType}</div>
-        </div>
-        <div className='px-4 py-2 rounded-md bg-gray-800 text-lg'>
-          Premium ${position.premiumPaid}
-        </div>
-      </div>
+    
+    <div className="flex justify-between items-start mt-2">
+  <div className="space-y-2">
+  <div>
+      <div className='text-white text-lg font-semibold'>${position.strikePrice}</div>
+      <div className='text-gray-300 text-sm'>Strike Price</div>
+    </div>
+    <div>
+      <div className='text-white text-lg font-semibold'>{position.quantity}</div>
+      <div className='text-gray-300 text-sm'>Quantity</div>
+    </div>
+    <div>
+      <div className='text-white text-lg font-semibold'>{position.expiration}</div>
+      <div className='text-gray-300 text-sm'>Expires on</div>
+    </div>
+    <div>
+      <div className='text-white text-lg font-semibold'>{position.positionType}</div>
+      <div className='text-gray-300 text-sm'>Position Type</div>
+    </div>
+  </div>
+  <div className='bg-gray-700 p-2 rounded-md'>
+    <div className='text-white lg:text-lg sm:text-xs font-semibold'>${position.premiumPaid}</div>
+    <div className='text-gray-300 text-sm'>Premium</div>
+  </div>
+</div>
+
+      
       <div className="mt-2">
         {position.positionType === 'Bought' && activeTab == "active" ? (
           <button className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded" onClick={() => onExecuteClick(position.contractAddr, position.type === 'CALL')}>
@@ -98,21 +114,22 @@ const PositionsPage = () => {
           </button>
         ) : ""}
       </div>
+      </div>
     </div>
-    </div>
+
   );
 
   return (
-    <div className="p-4 md:p-8 bg-black min-h-full mt-32 m-8">
+    <>
       {loading && <LoadingScreen message={loadingMessage} />}
       {!loading && (
-        <>
+        <div className="p-4 md:p-8 min-h-full mt-32 m-8">
           <div className="mb-4 border-b border-gray-700">
             <ul className="flex cursor-pointer text-sm font-medium text-center text-gray-500">
-              <li className={`flex-1 p-4 ${activeTab === 'active' ? 'text-blue-500 border-b-2 border-blue-500' : 'text-gray-500'}`} onClick={() => setActiveTab('active')}>
+              <li className={`flex-1 text-lg p-4 ${activeTab === 'active' ? 'text-emerald-500 border-b-2 border-emerald-500' : 'text-gray-500'}`} onClick={() => setActiveTab('active')}>
                 Active Positions
               </li>
-              <li className={`flex-1 p-4 ${activeTab === 'closed' ? 'text-blue-500 border-b-2 border-blue-500' : 'text-gray-500'}`} onClick={() => setActiveTab('closed')}>
+              <li className={`flex-1 text-lg p-4 ${activeTab === 'closed' ? 'text-emerald-500 border-b-2 border-emerald-500' : 'text-gray-500'}`} onClick={() => setActiveTab('closed')}>
                 Closed Positions
               </li>
             </ul>
@@ -120,9 +137,9 @@ const PositionsPage = () => {
           <div>
             {activeTab === 'active' ? positionsData.active.map(renderPosition) : positionsData.closed.map(renderPosition)}
           </div>
-        </>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
