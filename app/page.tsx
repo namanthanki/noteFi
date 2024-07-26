f46daea36430f4a6288c3c7a4f3849327d2e326a
Vraj <68881630+vking45@users.noreply.github.com>
2024-07-26 22:56:52 +0530

    Connected Check & Strict Mode (#41)
    Resolves #37

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import HeroSection from '@/components/HeroSection';
-import { DatePicker } from 'antd';
 import FeaturesSection from "@/components/FeaturesSection";
-import png from "@/public/magicpattern-grid-pattern-1720160852088.png"
 import FAQSection from "@/components/FAQs";
 import Footer from "@/components/Footer";
 import TermsModal from "@/components/TermsModal";
